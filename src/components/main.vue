f6632f78718a9d4d56b1e52091d6047714c0661a
hehe <xiaolaji@qq.com>
2021-01-06 11:36:52 +0800

    aa

---
+++ src/components/main.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="hello">
+   where there is a will,there is a way!
+  </div>
+</template>
+
+<script>
+export default {
+ 
+  data () {
+    return {
+     
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.hello{
+	font-size:"50px";
+	color:"skyblue";
+}
+</style>
